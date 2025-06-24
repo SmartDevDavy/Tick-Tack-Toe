425dc31c12840908c8998566d8e6c92af9ff7931
SmartDevDavy <davykamau8@gmail.com>
2025-06-24 08:44:45 +0300

    Trial

---
+++ tick.js
@@ -0,0 +1,24 @@
+
+
+function Gameboard() {
+    this.gameboard = [];
+
+}
+
+//This is a player constructor
+function Player(name, marker){
+this.name =  name;
+this.marker = marker;
+}
+
+
+//IIFE just for practice not needed
+/*
+let store =  (function(){
+    let myStore = ["Apple", "Banana", "Mango"];
+    return myStore;
+}) ();
+
+console.log(store[1]);
+*/
+
